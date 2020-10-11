657d2274a4d338b912962d3f83300a019f305202
Jordan Muncrief <jordean89@gmail.com>
2020-10-11 08:38:23 -0600

    Installed react-bootstrap. Initilized Router and created home and 404 pages.

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+const Home = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Home Page</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
