8040dac3355fb70da48546104fab4525f41a82fb
Rachel Sakles <rachel.sakles@yahoo.com>
2020-10-12 16:36:09 -0700

    added styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { Button, Form, FormControl, Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import { Button, Form, FormControl, Navbar, NavDropdown, Nav } from 'react-bootstrap'; 
+import "./Header.css";
 
 
 
 function Header() {
     return (
-        <Navbar bg="dark" expand="lg" variant="dark">
+        <Navbar bg="" expand="lg" class="navbar">
             <Navbar.Brand href="#home" id="title">Cocktail Lounge</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Favorites</Nav.Link>
-                    <NavDropdown title="Sort by liquor" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Vodka</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Rum</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Gin</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Tequila</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Scotch</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                    <Nav.Link href="#home" className="hpLink">Favorites</Nav.Link>
+                    <NavDropdown title="Sort by liquor" id="nav-dropdown">
+                        <NavDropdown.Item className="liquors" href="#action/3.1">Vodka</NavDropdown.Item>
+                        <NavDropdown.Item className="liquors" href="#action/3.2">Rum</NavDropdown.Item>
+                        <NavDropdown.Item className="liquors" href="#action/3.3">Gin</NavDropdown.Item>
+                        <NavDropdown.Item className="liquors" href="#action/3.3">Tequila</NavDropdown.Item>
+                        <NavDropdown.Item className="liquors" href="#action/3.3">Scotch</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
                 <Nav className="">
@@ -26,7 +25,7 @@ function Header() {
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                         <Button variant="outline-success">Search</Button>
                     </Form >
-                    <Nav.Link href="#login" style={{ justifyContent: "end" }}>Sign-In</Nav.Link>
+                    <Nav.Link href="#login" className="hpLink" style={{ justifyContent: "end" }}>Sign-In</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar >
