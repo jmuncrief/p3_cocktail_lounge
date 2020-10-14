b4013bac03205ca27e5fe646139851b00d03bcd5
cseibert2667 <cseibert2667@gmail.com>
2020-10-14 14:35:26 -0700

    login/members page working

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,16 +1,11 @@
 import React, { useRef } from "react";
-<<<<<<< HEAD
 import { Button, Form, FormControl, Navbar, NavDropdown, Nav, Dropdown, DropdownButton } from 'react-bootstrap';
-=======
-import { Button, Form, FormControl, Navbar, NavDropdown, Nav } from 'react-bootstrap'; 
->>>>>>> d230aaf263d289cd46d9b98c3f7ca397a99600b4
 import "./Header.css";
 import API from "../../utils/axiosCalls"
 
 
 
 function Header() {
-<<<<<<< HEAD
 
     // Refs to search field and dropdown
     const inputRef = useRef();
@@ -47,21 +42,13 @@ function Header() {
     }
 
 
-=======
-    const inputRef = useRef();
-
-    function search(val) {
-        
-    }
-
->>>>>>> d230aaf263d289cd46d9b98c3f7ca397a99600b4
     return (
         <Navbar bg="" expand="lg" class="navbar">
             <Navbar.Brand href="/home" id="title">Cocktail Lounge</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home" className="hpLink">Favorites</Nav.Link>
+                    <Nav.Link href="/home" className="hpLink">Favorites</Nav.Link>
                     <NavDropdown title="Sort by liquor" id="nav-dropdown">
                         <NavDropdown.Item className="liquors" href="#action/3.1">Vodka</NavDropdown.Item>
                         <NavDropdown.Item className="liquors" href="#action/3.2">Rum</NavDropdown.Item>
@@ -73,7 +60,6 @@ function Header() {
                 <Nav className="">
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" ref={inputRef} />
-<<<<<<< HEAD
 
                         <DropdownButton alignRight title="Search for:" id="search-dropdown"
                             ref={typeRef} onSelect={searchSelect}>
@@ -83,11 +69,8 @@ function Header() {
 
                         <Button onClick={() => search(inputRef.current.value)} variant="outline-success">Search</Button>
                         <Button onClick={() => randSearch()} variant="outline-success">Random</Button>
-=======
-                        <Button variant="outline-success" onClick={() => search(inputRef)}>Search</Button>
->>>>>>> d230aaf263d289cd46d9b98c3f7ca397a99600b4
                     </Form >
-                    <Nav.Link href="/logins" className="hpLink" style={{ justifyContent: "end" }}>Sign-In</Nav.Link>
+                    <Nav.Link href="/login" className="hpLink" style={{ justifyContent: "end" }}>Sign-In</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar >
