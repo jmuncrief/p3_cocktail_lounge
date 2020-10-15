548fa490528ae59c5fbe90fb1c86a583412f9633
Jordan Muncrief <jordean89@gmail.com>
2020-10-15 13:02:31 -0600

    Moved search from navbar/header into search component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,31 +1,59 @@
-import React from "react"; 
-import { Button, FormControl, Form } from "react-bootstrap"; 
+import React, { useRef, useState } from "react";
+import { Button, Form, FormControl, Navbar, NavDropdown, Nav, Dropdown, DropdownButton } from 'react-bootstrap';
 import "./Search.css"
+import API from "../../utils/axiosCalls"
 
 function Search() {
+
+    const [searchState, setSearchState] = useState({
+        searchType: "Drink Name",
+        dropdownTitle: "Search for:",
+        query: ""
+
+    })
+
+    // Runs when dropdown value is changed; changes searchType
+    function searchSelect(e) {
+        setSearchState({ ...searchState, searchType: e, dropdownTitle: e })
+    }
+
+    async function search(query) {
+        switch (searchState.searchType) {
+            case "Ingredient": {
+                const { data } = await API.filterIngredient(query)
+                console.log(data.drinks)
+            } break;
+            case "Drink Name": {
+                const { data } = await API.searchCocktailName(query)
+                console.log(data.drinks)
+            } break;
+            default:
+                break;
+        }
+
+    }
+
+    async function randSearch() {
+        const { data } = await API.random()
+        console.log(data.drinks[0])
+    }
+
+
     return (
         <div>
             <Form inline className="searchComp">
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => setSearchState({ ...searchState, query: e.target.value })} />
 
-                <div key="default-checkbox" className="mb-3 checkboxes">
-                    <Form.Check
-                        type="checkbox"
-                        id="default-checkbox"
-                        label="Search by Name"
-                    />
-                    <Form.Check
-                        type="checkbox"
-                        id="default-checkbox"
-                        label="Search by Ingredient"
-                    />
-                </div>  
-                <div> 
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
-                </div>
+                <DropdownButton alignRight title={searchState.dropdownTitle} id="search-dropdown" onSelect={searchSelect}>
+                    <Dropdown.Item eventKey="Drink Name">Drink Name</Dropdown.Item>
+                    <Dropdown.Item eventKey="Ingredient">Ingredient</Dropdown.Item>
+                </DropdownButton>
+
+                <Button onClick={() => search(searchState.query)} variant="outline-success">Search</Button>
+                <Button onClick={() => randSearch()} variant="outline-success">Random</Button>
 
             </Form>
-        </div> 
+        </div>
     )
 
 }
