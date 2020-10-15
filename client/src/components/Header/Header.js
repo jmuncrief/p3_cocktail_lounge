8a97bd84e0944cb2d8a568ff7023b83ae3bfa542
Jordan Muncrief <64342821+jmuncrief@users.noreply.github.com>
2020-10-15 13:09:56 -0600

    Jordan (#23)
    * Changed API route to '/users/signup'
    
    * Moved search from navbar/header into search component
    
    * Fixed login/logins href on sign-in button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,40 +5,6 @@ import API from "../../utils/axiosCalls"
 
 function Header() {
 
-    const [headerState, setHeaderState] = useState({
-        searchType: "Drink Name",
-        dropdownTitle: "Search for:",
-        query: ""
-
-      })
-
-    // Runs when dropdown value is changed; changes searchType
-    function searchSelect(e) {
-        setHeaderState({...headerState, searchType: e, dropdownTitle: e})
-    }
-
-    async function search(query) {
-        switch (headerState.searchType) {
-            case "Ingredient": {
-                const { data } = await API.filterIngredient(query)
-                console.log(data.drinks)
-            } break;
-            case "Drink Name": {
-                const { data } = await API.searchCocktailName(query)
-                console.log(data.drinks)
-            } break;
-            default:
-                break;
-        }
-
-    }
-
-    async function randSearch() {
-        const { data } = await API.random()
-        console.log(data.drinks[0])
-    }
-
-
     return (
         <Navbar bg="" expand="lg" class="navbar">
             <Navbar.Brand href="/home" id="title">Cocktail Lounge</Navbar.Brand>
@@ -55,18 +21,7 @@ function Header() {
                     </NavDropdown>
                 </Nav>
                 <Nav className="">
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => setHeaderState({...headerState, query: e.target.value})} />
-
-                        <DropdownButton alignRight title={headerState.dropdownTitle} id="search-dropdown" onSelect={searchSelect}>
-                            <Dropdown.Item eventKey="Drink Name">Drink Name</Dropdown.Item>
-                            <Dropdown.Item eventKey="Ingredient">Ingredient</Dropdown.Item>
-                        </DropdownButton>
-
-                        <Button onClick={() => search(headerState.query)} variant="outline-success">Search</Button>
-                        <Button onClick={() => randSearch()} variant="outline-success">Random</Button>
-                    </Form >
-                    <Nav.Link href="/login" className="hpLink" style={{ justifyContent: "end" }}>Sign-In</Nav.Link>
+                    <Nav.Link href="/logins" className="hpLink" style={{ justifyContent: "end" }}>Sign-In</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar >
