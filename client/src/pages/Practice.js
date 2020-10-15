dd17a587984a06630bb70b54a3c59f4708a539fd
rachelsakles <68452205+rachelsakles@users.noreply.github.com>
2020-10-15 11:51:18 -0700

    card changes (#22)

---
+++ client/src/pages/Practice.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cocktail from "../components/Card/Card"
+import { Container, Wrapper } from 'react-bootstrap'
+
+
+export default function Practice() {
+    return (
+
+        <Container>
+
+            <Cocktail />
+
+         </Container>
+
+
+    )
+}
