c9b2f3bc3fc4799e31b05a83afe3734f68fdf905
Jordan Muncrief <64342821+jmuncrief@users.noreply.github.com>
2020-10-16 14:38:16 -0600

    Jordan (#31)
    * Changed API route to '/users/signup'
    
    * Moved search from navbar/header into search component
    
    * Fixed login/logins href on sign-in button
    
    * Issues with updating state in search function. Creating problem and assigning Tucker for assistance
    
    * Set new state for results and moved console.logs. Search data is being written to state. I may now owe Tucker a beer
    
    * committing small changes in preparation of pull
    
    * Working on render of cards to page. Commiting to pull
    
    * Search now renders cocktail cards to teh page

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -4,16 +4,20 @@ import Cocktail from "../Card/Card"
 import "./Search.css"
 import API from "../../utils/axiosCalls"
 function Search() {
+
     const [searchState, setSearchState] = useState({
         searchType: "Drink Name",
         dropdownTitle: "Search for:",
         query: "",
     })
+
     const [results, setResults] = useState([])
+
     // Runs when dropdown value is changed; changes searchType
     function searchSelect(e) {
         setSearchState({ ...searchState, searchType: e, dropdownTitle: e })
     }
+
     async function search(query) {
         switch (searchState.searchType) {
             case "Ingredient": {
@@ -28,33 +32,34 @@ function Search() {
                 break;
         }
     }
+
     async function randSearch() {
         const { data } = await API.random()
         console.log(data.drinks[0])
     }
+
     return (
         <>
-        <div>
-            <Form inline className="searchComp">
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => setSearchState({ ...searchState, query: e.target.value })} />
-                <DropdownButton variant="danger" alignRight title={searchState.dropdownTitle} id="search-dropdown" onSelect={searchSelect}>
-                    <Dropdown.Item eventKey="Drink Name">Drink Name</Dropdown.Item>
-                    <Dropdown.Item eventKey="Ingredient">Ingredient</Dropdown.Item>
-                </DropdownButton>
-                <Button className="search-btn" onClick={() => search(searchState.query)} variant="outline-success">Search</Button>
-                <Button className="search-btn" onClick={() => randSearch()} variant="outline-success">Random</Button>
-            </Form>
-        </div>
-        <div>
-            {
-                results.forEach(element => {
-                    return(
-                        // console.log(element.idDrink, element.strDrink, element.strDrinkThumb)
-                        <Cocktail id={element.idDrink} name={element.strDrink} img={element.strDrinkThumb}/>
-                    )
-                })
-            }
-        </div>
+            <div>
+                <Form inline className="searchComp">
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => setSearchState({ ...searchState, query: e.target.value })} />
+                    <DropdownButton variant="danger" alignRight title={searchState.dropdownTitle} id="search-dropdown" onSelect={searchSelect}>
+                        <Dropdown.Item eventKey="Drink Name">Drink Name</Dropdown.Item>
+                        <Dropdown.Item eventKey="Ingredient">Ingredient</Dropdown.Item>
+                    </DropdownButton>
+                    <Button className="search-btn" onClick={() => search(searchState.query)} variant="outline-success">Search</Button>
+                    <Button className="search-btn" onClick={() => randSearch()} variant="outline-success">Random</Button>
+                </Form>
+            </div>
+            <div>
+                {
+                    results.map(element => (
+
+                        <Cocktail id={element.idDrink} name={element.strDrink} img={element.strDrinkThumb} />
+
+                    ))
+                }
+            </div>
         </>
     )
 }
