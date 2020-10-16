70e278942460b577cdfbda747f6575931ec45843
rachelsakles <68452205+rachelsakles@users.noreply.github.com>
2020-10-16 11:26:44 -0700

    Rachel (#27)
    * card changes
    
    * fixed footer, and added on members page
    
    * moved search bar into own component, and adjusted styling
    
    * fixed list

---
+++ client/src/components/FavoritesList/FavoritesList.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import {ListGroup} from 'react-bootstrap'
+
+import "./FavoritesList.css"
+
+function Fav() {
+    return (  
+        <>
+        <h2 className="favorites-title">My Favorites</h2>
+        <ListGroup className="fav-box" defaultActiveKey="#link1">
+            <ListGroup.Item className="fav-list" action href="#link1">
+                Margarita
+            </ListGroup.Item>
+        </ListGroup> 
+        </>
+    );
+
+
+}
+
+export default Fav;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,25 @@
 import React from "react";
-import Slides from "../components/Slides/Slides";  
-import { Row, Container, Col } from "react-bootstrap"; 
+import Slides from "../components/Slides/Slides";
+import { Row, Container, Col } from "react-bootstrap";
 import Search from "../components/Search/Search"
 
 
 const Home = () => {
-  return (  
-    <Container fluid>
-    <Row>
-      <Col>
-        <Slides/>
-      </Col>
-    </Row> 
-    <Row> 
-      <Search/>
-    </Row>
-  </Container>
+  return (
+   
+    <Container fluid style={{height:"100vh"}}>
+      <Row>
+        <Col>
+          <Slides />
+        </Col>
+      </Row>  
+      <Row style={{ padding:"20px", justifyContent:"center"}}>
+        <Search />
+      </Row>
+      <div style={{height:"50px" }}></div>
+    </Container>
+  
+
   );
 };
 
