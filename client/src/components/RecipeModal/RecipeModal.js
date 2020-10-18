16be37c40999b8b86b75da6adc6cddd75d47fbd6
Jordan Muncrief <jordean89@gmail.com>
2020-10-18 10:19:23 -0600

    Updated sanitizeIngredients with new function that returns results as array instead of object. Replaced called function in recipe modal with new function

--- client/src/components/RecipeModal/RecipeModal.js
+++ client/src/components/RecipeModal/RecipeModal.js
@@ -3,6 +3,7 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import API from "../../utils/API"
 import sanitizeIngredients from "../../utils/sanitizeIngredients"
+import sanitizeIngredientsIntoArray from "../../utils/sanitizeIngredients"
 
 function RecipeModal(props) {
     const [show, setShow] = useState(false);
@@ -24,7 +25,7 @@ function RecipeModal(props) {
             alcoholic: isAlc(),
             imageURL: data.strDrinkThumb,
             instructions: data.strInstructions,
-            ingredients: sanitizeIngredients(data),
+            ingredients: sanitizeIngredientsIntoArray(data),
         }).then((result) => {
             console.log(result)
         })
--- client/src/utils/sanitizeIngredients.js
+++ client/src/utils/sanitizeIngredients.js
@@ -8,3 +8,14 @@ function sanitizeIngredients(data) {
     }
     return ingredients;
 }
+
+function sanitizeIngredientsIntoArray(data) {
+  const ingredients = []
+
+  for (let i=1; i < 16; i++) {
+    const ing = "strIngredient" + i
+    if(data[ing]) {
+      ingredients[i] = data[ing] + " - " + data["strMeasure" + i]
+    }
+  }
+}
