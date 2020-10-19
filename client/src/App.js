73c96f09165001f5760a09e4a8c824d823a373f8
Rachel Sakles <rachel.sakles@yahoo.com>
2020-10-18 17:53:24 -0700

    deleted practice

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 //caused the site to not compile in the browser. Unsure as to why as of yet.
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Logins from "./components/LoginSign/Logins";
 import RecipeJumbo from "./components/RecipeJumbo/RecipeJumbo";
 import "./App.css"
@@ -15,7 +14,6 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Members from "./pages/Members";
 import PrivateRoute from "./utils/PrivateRoute";
-import Practice from "./pages/Practice"
 
 //this router function is from react and is how we navigate between pages through the specified routes
 function App() {
@@ -27,7 +25,6 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/logins" component={Login} />
-          <Route exact path="/practice" component={Practice} />
           <PrivateRoute exact path="/members" component={Members} />
           <Route exact path="/recipejumbo" component={RecipeJumbo} />
           <Route component={NoMatch} />
